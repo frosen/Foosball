50aa67c996fb89b49b4c61c07ff4b9e72f49dcf6
卢乐颜 <frosen_lu@126.com>
2016-07-10 22:08:14 +0800

    第一次提交

---
+++ Foosball/src/Bridging.h
@@ -0,0 +1,15 @@
+
+//  Bridging.h
+//  TheOneFoosball
+//
+//  Created by 卢乐颜 on 16/4/18.
+//  Copyright © 2016年 luleyan. All rights reserved.
+//
+
+#ifndef Bridging_h
+#define Bridging_h
+
+#import <AVOSCloud/AVOSCloud.h> //
+//#import "MJRefresh.h" //
+
+#endif /* Bridging_h */
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Foosball' do
+
+pod 'AVOSCloud'
+pod 'MJRefresh'
+pod 'SnapKit'
+
+end
+
