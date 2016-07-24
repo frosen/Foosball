fa2de6bf2fffe9c141212cdc56c06384846be7c5
卢乐颜 <frosen_lu@126.com>
2016-07-24 21:36:24 +0800

    开始制作个人页面，试了下位置，ok，学习了给image裁剪后加阴影，加点击事件等，下次把InfoHeadView重新写为一个view

--- Foosball/src/ui/own/OwnCells.swift
+++ Foosball/src/ui/own/OwnCells.swift
@@ -9,43 +9,78 @@
 import UIKit
 
 class OwnTitleCell: UITableViewCell {
+    var bg: UIImageView? = nil
+    var avatar: UIImageView? = nil
+    var title: UILabel? = nil
+    var subTitle: UILabel? = nil
+
+    var leftView: UIView? = nil
+    var rightView: UIView? = nil
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        let w = Int(UIScreen.mainScreen().bounds.width)
         let h = Int(OwnTitleCell.getCellHeight())
 
         //背景
-        let bg = UIImageView(image: UIImage(named: "selfbg"))
-        contentView.addSubview(bg)
-        bg.frame = CGRect(x: 0, y: 0, width: Int(UIScreen.mainScreen().bounds.width), height: h)
+        bg = UIImageView(image: UIImage(named: "selfbg"))
+        contentView.addSubview(bg!)
+        bg!.frame = CGRect(x: 0, y: 0, width: w, height: h)
+        bg!.contentMode = .ScaleAspectFill
 
         //头像
-        let avatar = UIImageView(image: UIImage(named: "default_avatar"))
-        contentView.addSubview(avatar)
-        let avatarW: CGFloat = 90
-        avatar.snp_makeConstraints { make in
+        avatar = UIImageView(image: UIImage(named: "default_avatar"))
+        contentView.addSubview(avatar!)
+        let avatarW: CGFloat = 70
+        avatar!.snp_makeConstraints{ make in
             make.size.equalTo(CGSize(width: avatarW, height: avatarW))
             make.centerX.equalTo(contentView)
-            make.centerY.equalTo(contentView)
+            make.centerY.equalTo(contentView).offset(-13.0)
         }
 
-        avatar.layer.cornerRadius = avatarW / 2 //圆形
-        avatar.layer.masksToBounds = true //剪切掉边缘以外
-        avatar.layer.borderColor = UIColor.purpleColor().CGColor
-        avatar.layer.borderWidth = 1.5
+        avatar!.layer.cornerRadius = avatarW / 2 //圆形
+        avatar!.layer.masksToBounds = true //剪切掉边缘以外
 
-        let nameView = UIView()
-        contentView.addSubview(nameView)
-        nameView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
-        let nameVW: CGFloat = 270
-        let nameVH: CGFloat = 30
-        nameView.snp_makeConstraints { make in
-            make.size.equalTo(CGSize(width: nameVW, height: nameVH))
+        avatar!.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).CGColor
+        avatar!.layer.borderWidth = 1.5
+
+        //左侧标志
+
+        //右侧标志
+
+        //名字
+        title = UILabel()
+        contentView.addSubview(title!)
+        title!.snp_makeConstraints{ make in
+            make.size.equalTo(CGSize(width: w, height: Int(Double(h) * 0.2)))
             make.centerX.equalTo(contentView)
-            make.bottom.equalTo(bg.snp_bottom).offset(-5)
+            make.centerY.equalTo(contentView.snp_bottom).offset(-43.0)
         }
-        nameView.layer.cornerRadius = nameVH / 3 //圆形
-        nameView.layer.masksToBounds = true
+        title!.textAlignment = NSTextAlignment.Center
+        title!.font = UIFont.systemFontOfSize(14.0)
+        title!.text = "聂小倩"
+        title!.textColor = UIColor.whiteColor()
+
+        title!.shadowColor = UIColor.blackColor()
+        title!.shadowOffset = CGSize(width: 1, height: 1)
+
+        //签名
+        subTitle = UILabel()
+        contentView.addSubview(subTitle!)
+        subTitle!.snp_makeConstraints{ make in
+            make.size.equalTo(CGSize(width: w, height: Int(Double(h) * 0.1)))
+            make.centerX.equalTo(contentView)
+            make.centerY.equalTo(contentView.snp_bottom).offset(-24.0)
+        }
+        subTitle!.textAlignment = NSTextAlignment.Center
+        subTitle!.font = UIFont.systemFontOfSize(11.0)
+        subTitle!.text = "个性签名，啦啦啦"
+        subTitle!.textColor = UIColor.whiteColor()
+
+        subTitle!.shadowColor = UIColor.blackColor()
+        subTitle!.shadowOffset = CGSize(width: 1, height: 1)
+
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -53,8 +88,16 @@ class OwnTitleCell: UITableViewCell {
     }
 
     class func getCellHeight() -> CGFloat {
-        return 200
+        return 160
     }
+
+//    func setScrollView(scrollView: UIScrollView) {
+//
+//    }
+//
+//    func onUpdateSrollOffsetY(newOffsetY: Float) {
+//
+//    }
 }
 
 class OwnQRCell: UITableViewCell {
