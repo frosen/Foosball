12fa4b909ddf681c66bc996531cb1ba0eafa67a7
luleyan <luleyan@xiaoyezi.com>
2016-12-29 17:19:58 +0800

    要做：toolbar的更简单样式，自定义颜色nav，检测数据变化，

--- Foosball/src/ui/search/SearchController.swift
+++ Foosball/src/ui/search/SearchController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
-class SearchController: BaseController {
+class SearchController: BaseController, UISearchBarDelegate {
+
+    var search: UISearchBar! = nil
 
     override func viewDidLoad() {
+        navTabType = .HideTab
         super.viewDidLoad()
 
         navigationItem.leftBarButtonItem = UITools.createBarBtnItem(self, action: #selector(SearchController.onBack), image: "go_back")
+
+        search = UISearchBar()
+        navigationItem.titleView = search
+
+        search.placeholder = "搜索"
+        search.delegate = self
+
+        // 点击画面收起输入框
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SearchController.endInput))
+        view.addGestureRecognizer(tap)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        search.becomeFirstResponder()
     }
 
     func onBack() {
+        endInput()
         let _ = navigationController?.popViewController(animated: true)
+    }
+
+    func endInput() {
+        search.resignFirstResponder()
     }
 }
--- Foosball/src/ui/widget/ActionBtnBoard.swift
+++ Foosball/src/ui/widget/ActionBtnBoard.swift
@@ -8,7 +8,63 @@
 
 import UIKit
 
+class AcBtn {
+    let text: String
+    init(t: String) {
+        text = t
+    }
+}
+
+class StateAction {
+    let color: UIColor
+    let lbtn: AcBtn
+    var rbtn: AcBtn? = nil
+    init(c: UIColor, l: AcBtn, r: AcBtn?) {
+        color = c
+        lbtn = l
+        rbtn = r
+    }
+}
+
 class ActionBtnBoard: UIView {
+
+    private static let stateActionList: [EventState: StateAction] = [
+        .invite: StateAction(c: UIColor.red,
+            l: AcBtn(t: "加入"),
+            r: AcBtn(t: "取消")),
+        .ready: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .ongoing: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .waiting: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .win: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .lose: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .honoured: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .finish: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .impeach: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+        .keepImpeach: StateAction(c: UIColor.red,
+            l: AcBtn(t: ""),
+            r: AcBtn(t: "")),
+    ]
+
+    private let margin: CGFloat = 15
+    private let topMargin: CGFloat = 3
+
+    private var btnWidth: CGFloat = 0
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -16,5 +72,37 @@ class ActionBtnBoard: UIView {
 
     init() {
         super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 36))
+    }
+
+    // 根据不同的状态产生不同的按钮
+    func setState(_ st: EventState) {
+        let stateAct: StateAction = ActionBtnBoard.stateActionList[st]!
+
+        // 计算按钮宽度
+        btnWidth = frame.width - 2 * margin
+        if stateAct.rbtn != nil {
+            btnWidth = (btnWidth - margin) / 2
+        }
+
+        let lBtn = UIButton(type: .custom)
+        addSubview(lBtn)
+        lBtn.frame = CGRect(
+            x: margin,
+            y: topMargin,
+            width: btnWidth,
+            height: frame.height - 2 * topMargin
+        )
+        lBtn.setTitleColor(UIColor.white, for: .normal)
+        lBtn.titleLabel!.font = UIFont.systemFont(ofSize: 14)
+        lBtn.backgroundColor = BaseColor
+        lBtn.layer.cornerRadius = 3
+
+        lBtn.setTitle(stateAct.lbtn.text, for: .normal)
+        
+    }
+
+    // 设置后会在右边出现聊天提示，并有气泡提示有多少条，如果小于等于0则消失
+    func setMsgTip(_ num: Int) {
+
     }
 }
